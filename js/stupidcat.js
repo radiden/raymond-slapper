68b4ee022ddeeb679d0cf6f109177d815352fa88
rainyuu <gabriel2012688@gmail.com>
2020-04-05 23:27:17 +0200

    now in dark! now actually has a font! and actually centered! and saves scores! what more could you want?

--- js/stupidcat.js
+++ js/stupidcat.js
@@ -1,39 +1,57 @@
-var slapcount = 0;
+if (typeof localStorage["slapcount"] === "undefined") {
+	localStorage["slapcount"] = 0;
+}
+slapcountint = parseInt(localStorage["slapcount"]);
 var slapcountlast = 0;
 var slap = new Audio("audio/slap.mp3");
 
 slap.volume = 0.2;
 
-function updatescore() {	
+function updatescore() {
 	var scorediv = document.getElementById("slapcount");
-	scorediv.innerHTML = `<strong>slap count: ${slapcount}</strong>`; 
+	scorediv.innerHTML = `<strong>slap count: ${localStorage["slapcount"]}</strong>`; 
+	checkforfunnysecret(slapcountint);
 }
 
 function stupidcat() {
 	funnyslap();
-	slapcount++;
+	slapcountint++;
 	slap.play();
+	slapcountlast = slapcountint;
+	localStorage["slapcount"] = slapcountint;
 	updatescore();
-	slapcountlast = slapcount;
-	checkforfunnysecret(slapcount, slapcountlast);
+	checkforhax(slapcountlast);
 }
 
-function checkforfunnysecret(slapcount, slapcountlast) {
+function checkforhax(slapcountlast) {
 	var amusingsecretdiv = document.getElementById("stupidcismalecat");
-	if (slapcount != slapcountlast++) {
-		amusingsecretdiv.innerHTML = `dirty cheater.`
+	if (slapcountint != slapcountlast++) {
+		amusingsecretdiv.innerHTML = `dirty cheater.`;
 	}
-	if (slapcount == 100) {
-		amusingsecretdiv.innerHTML = `wow, you really do hate raymond. good.`
+}
+
+function checkforfunnysecret(slapcount) {
+	var amusingsecretdiv = document.getElementById("stupidcismalecat");	
+	if (slapcount >= 10000) {
+		amusingsecretdiv.innerHTML = `please stop`;
 	}
-	else if (slapcount == 200) {
-		amusingsecretdiv.innerHTML = `raymond is dead now you may stop`
+	else if (slapcount >= 2000) {
+		amusingsecretdiv.innerHTML = `i am actually impressed`;
 	}
-	else if (slapcount == 201) {
-		amusingsecretdiv.innerHTML = `haha just kidding`
+	else if (slapcount >= 1000) {
+		amusingsecretdiv.innerHTML = `i think this is not healthy please stop`;
 	}
-	else if (slapcount == 1000) {
-		amusingsecretdiv.innerHTML = `i think this is not healthy please stop`
+	else if (slapcount >= 202) {
+		amusingsecretdiv.innerHTML = `slap cat`;
+	}
+	else if (slapcount === 201) {
+		amusingsecretdiv.innerHTML = `haha just kidding`;
+	}
+	else if (slapcount === 200) {
+		amusingsecretdiv.innerHTML = `raymond is dead now you may stop`;
+	}
+	else if (slapcount >= 100) {
+		amusingsecretdiv.innerHTML = `wow, you really do hate raymond. good.`;
 	}
 }
 
